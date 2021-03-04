6cc0b2b45dea7bf32f0360770644cd07c671da70
thesmallstar <manthan.surkar@gmail.com>
2021-03-04 21:46:57 +0530

    array

--- cards/main.go
+++ cards/main.go
@@ -1,12 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	card, card2 := newCard()
-	fmt.Println(card2 + card)
+	//card, card2 := newCard()
+	//fmt.Println(card2 + card)
+	array()
 }
 
-func newCard() (string, string) {
-	return "Five of Diamonds", "hello"
+func newCard() string {
+	return "Five of Diamonds"
+}
+
+func array() {
+	cards := []string{newCard(), "Five of diamonds"}
+	cards = append(cards, "Six of spades")
+
+	for _, card := range cards {
+		fmt.Println(card)
+	}
+
+	fmt.Println(cards)
 }
