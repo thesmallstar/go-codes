9084dc2361b811fcbda5bc865e076b14331cf08f
thesmallstar <manthan.surkar@gmail.com>
2021-03-06 02:05:50 +0530

    interfaces

---
+++ cards/maps/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf75",
+	// }
+	// var colors2 map[string]string
+
+	colors := make(map[string]string)
+	colors["white"] = "#ffffff"
+
+}
